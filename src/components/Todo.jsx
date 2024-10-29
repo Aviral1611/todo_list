fd3fe998f77fba7817444a56085c1ec28d87ae8f
Aviral <aviral.vbansal16@gmail.com>
2024-10-29 20:55:00 +0530

    added the add and delete tasks functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
 
 const Todo = () => {
+
+const inputRef = useRef();
+
+
+const [todoList, setTodoList] = useState([]);
+
+const add = () => {
+      const inputText = inputRef.current.value.trim();
+      
+      if (inputText === ''){
+        return;
+      }
+      const newTodo = {
+        id: Date.now(),
+        text: inputText,
+        isComplete: false,
+      }
+      setTodoList((prev) =>[...prev,newTodo]);
+      inputRef.current.value = '';
+}
+const handleKeyDown = (event) => {
+  if (event.key === 'Enter') {
+    add();
+  }
+};
+
+const deleteTodo = (id) => {
+    setTodoList((prevTodos)=>{
+         return prevTodos.filter((todo) => todo.id !== id)
+    })
+}
+
+const toggle = (id) => {
+  setTodoList((prevTodos) => {
+      return prevTodos.map((todo) =>{
+        if (todo.id === id) {
+          return {...todo, isComplete: !todo.isComplete}
+      }
+      return todo;
+      })
+  })
+}
+
+useEffect(() => {
+  console.log(todoList)
+},
+[todoList])
+
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
 
@@ -12,15 +61,19 @@ const Todo = () => {
         </div>
 
         <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-            <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder="Enter a task" />
-            <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>
+            <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder="Enter a task"  onKeyDown={handleKeyDown} />
+            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>
                 ADD +
             </button>
         </div>
 
         <div>
-          <TodoItems text="Learn Coding"/>
-          <TodoItems text="Do DSA"/>
+          {todoList.map((item,index) => {
+              return <TodoItems key={index} text={item.text} 
+              id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}
+              toggle = {toggle}/>
+          })}
+
         </div>
     </div>
   )
